ffc81ea9ec220cffca379e2755fdbff204d2a344
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-02-28 11:06:38 -0300

    Add initial tests

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup
 setup(
 	name='measuresoftgram',
 	version='0.0a1',
+
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'setuptools',
+            'wheel'
+        ]
+    },
+
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
---
+++ tests/hello_world_test.py
@@ -0,0 +1,9 @@
+# pytest unit hello world test
+import pytest
+
+import measuresoftgram.cli
+
+def test_hello_world():
+    # asset hello world was called
+    assert True
+    
