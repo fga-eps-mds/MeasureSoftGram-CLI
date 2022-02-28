a21b326f4299a2522a39c63fcaeecd88b1bc5555
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-02-28 11:28:45 -0300

    Add coverage

--- tests/hello_world_test.py
+++ tests/hello_world_test.py
@@ -1,9 +1,13 @@
 # pytest unit hello world test
 import pytest
 
-import measuresoftgram.cli
+from measuresoftgram import cli
 
 def test_hello_world():
     # asset hello world was called
-    assert True
+    assert cli.hello_world() == None
+
+def test_main():
+    # asset main was called
+    assert cli.main() == None
     
