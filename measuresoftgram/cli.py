7a9d96ae460fa0244286b342c1addc6dbcfcc143
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-02-28 10:26:05 -0300

    Add CLI base structure

---
+++ measuresoftgram/cli.py
@@ -0,0 +1,6 @@
+def hello_world():
+    print("Hello World!")
+
+def main():
+    """Entry point for the application script"""
+    hello_world()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='measuresoftgram',
+	version='0.0a1',
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'measuresoftgram=measuresoftgram.cli:main'
+        ]
+    },
+)
