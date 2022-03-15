24d3b10d227a755668788cdb53430b1c066e8d4e
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-03-15 11:17:51 -0300

    Fix lint problems

--- measuresoftgram/cli.py
+++ measuresoftgram/cli.py
@@ -1,12 +1,15 @@
 import argparse
 import sys
 
+
 def hello_world():
     print("Hello World!")
+
 
 def parse_import():
     print("Importing metrics")
     pass
+
 
 def parse_create():
     print("Creating a new pre conf")
@@ -16,10 +19,14 @@ def parse_create():
 def main():
     """Entry point for the application script"""
 
-    parser = argparse.ArgumentParser(description="Command line interface for measuresoftgram")
+    parser = argparse.ArgumentParser(
+        description="Command line interface for measuresoftgram"
+    )
     subparsers = parser.add_subparsers(help="sub-command help")
     parser_import = subparsers.add_parser("import", help="Import a metrics file")
-    parser_create = subparsers.add_parser("create", help="Create a new model pre configuration")
+    parser_create = subparsers.add_parser(
+        "create", help="Create a new model pre configuration"
+    )
 
     parser_import.set_defaults(func=parse_import)
     parser_create.set_defaults(func=parse_create)
