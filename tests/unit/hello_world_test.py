35e98dcb59370892d840e41f26a397877e2df444
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-03-18 00:42:31 -0300

    Fix unit tests due hello_world function exclusion

--- tests/unit/hello_world_test.py
+++ tests/unit/hello_world_test.py
@@ -1,8 +1,9 @@
 # pytest unit hello world test
+import pytest
 from measuresoftgram import cli
 
 
+@pytest.mark.skip(reason="tests wont't be empty")
 def test_hello_world():
     # asset hello world was called
     assert cli.hello_world() is None
-
