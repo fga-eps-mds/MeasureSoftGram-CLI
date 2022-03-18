a7674351a6e1f737229e1427099c7bb080608ade
Joao Henrique <jhcalzavara@hotmail.com>
2022-03-18 17:10:31 -0300

    Adds refactoring and validate functions
    Co-authored-by: LucasPimentel123 <luk.2001@hotmail.com>

--- measuresoftgram/cli.py
+++ measuresoftgram/cli.py
@@ -12,6 +12,8 @@ INVALID_WEIGHT_SUM_ERROR = "Invalid weights," + VALID_WEIGHT_SUM_ADVISE
 VALID_WEIGHT_ADVISE = "the weight must be between 0 and 100"
 INVALID_WEIGHT_VALUE = "Invalid weight, " + VALID_WEIGHT_ADVISE
 
+VALID_CHECKBOX_ERROR = "Select at least one Check-Box"
+
 
 def define_weight(data_key, data_name):
     while True:
@@ -50,6 +52,13 @@ def validate_weight_value(weight):
         return True
 
 
+def validate_checkBox_input(selected):
+    if selected > 0:
+        return True
+    else:
+        return False
+
+
 def sublevel_cli(level_name, level_alias, sublevels, available_pre_config):
     reverse_sublevel = {v["name"]: k for k, v in available_pre_config.items()}
 
@@ -76,23 +85,30 @@ def define_characteristic(available_pre_config):
         return user_characteristics, characteristics_weights
 
     reversed_characteristics = {v["name"]: k for k, v in characteristics.items()}
+    while True:
+        characteristics_answer = [
+            inquirer.Checkbox(
+                "characteristics",
+                message="Choose the characteristics",
+                choices=[x["name"] for _, x in characteristics.items()],
+            )
+        ]
 
-    characteristics_answer = [
-        inquirer.Checkbox(
-            "characteristics",
-            message="Choose the characteristics",
-            choices=[x["name"] for _, x in characteristics.items()],
+        user_characteristics = inquirer.prompt(
+            characteristics_answer, theme=GreenPassion()
         )
-    ]
 
-    user_characteristics = inquirer.prompt(characteristics_answer, theme=GreenPassion())
+        if validate_checkBox_input(len(user_characteristics["characteristics"])):
+            break
+        else:
+            print(VALID_CHECKBOX_ERROR)
 
     user_characteristics = [
         reversed_characteristics[x] for x in user_characteristics["characteristics"]
     ]
 
     if len(user_characteristics) == 1:
-        print("Only one characteristic selected, no need to define weights")
+        print("\nOnly one characteristic selected, no need to define weights")
         characteristics_weights = [{user_characteristics[0]: 100}]
         return user_characteristics, characteristics_weights
 
@@ -124,19 +140,32 @@ def define_measures(user_sub_characteristics, available_pre_config):
     selected_measures = []
     measures_weights = []
 
+    ## CONFERIR SE TA CERTO
+
     for x in user_sub_characteristics:
-        while True:
-            local_selected_measures = sublevel_cli(
-                available_pre_config["subcharacteristics"][x]["name"],
-                "measures",
-                available_pre_config["subcharacteristics"][x]["measures"],
-                available_pre_config["measures"],
+        if len(available_pre_config["subcharacteristics"][x]["measures"]) == 1:
+            local_selected_measures = available_pre_config["subcharacteristics"][x][
+                "measures"
+            ]
+            local_measures_weights = [{local_selected_measures[0]: 100}]
+            print(
+                f"\nOnly one measure (%s) available, no need to select or define weights\n"
+                % (available_pre_config["measures"][local_selected_measures[0]]["name"])
             )
 
-            if len(local_selected_measures) > 0:
-                break
-            else:
-                print("No measures selected")
+        else:
+            while True:
+                local_selected_measures = sublevel_cli(
+                    available_pre_config["subcharacteristics"][x]["name"],
+                    "measures",
+                    available_pre_config["subcharacteristics"][x]["measures"],
+                    available_pre_config["measures"],
+                )
+
+                if validate_checkBox_input(len(local_selected_measures)):
+                    break
+                else:
+                    print(VALID_CHECKBOX_ERROR)
 
         local_measures_weights = []
 
@@ -180,7 +209,7 @@ def define_subcharacteristics(user_characteristics, available_pre_config):
                 {local_selected_sub_characteristics[0]: 100}
             ]
             print(
-                f"Only one subcharacteristic (%s) available, no need to select or define weights"
+                f"\nOnly one subcharacteristic (%s) available, no need to select or define weights\n"
                 % (
                     available_pre_config["subcharacteristics"][
                         local_selected_sub_characteristics[0]
@@ -273,3 +302,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Refatoramos a mensagem de erro da caracteristica e sub-caracteristica para uma mensagem só para ambas (VALID_CHECKBOX_ERROR)
+# Fizemos um while para garantir que será selecionada ao menos uma característica(linha 88)
