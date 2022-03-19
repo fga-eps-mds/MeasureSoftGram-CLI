0d995ef2e70aaf23d0020dc92068aadf7db8b873
Rhuan Carlos <40495695+Rhuancpq@users.noreply.github.com>
2022-03-19 00:26:55 -0300

    Update version to v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="measuresoftgram",
-    version="0.0a2",
+    version="1.0.0",
     extras_require={"dev": ["pytest", "pytest-cov", "setuptools", "wheel"]},
     install_requires=["inquirer", "requests"],
     # To provide executable scripts, use entry points in preference to the
