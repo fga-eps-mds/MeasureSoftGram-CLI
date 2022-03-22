59cddf5e82610915dfa951e0b9c042394114e859
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-03-22 20:51:34 -0300

    Change inquirer to v2.8.0 due backspace bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="measuresoftgram",
     version="1.0.0",
     extras_require={"dev": ["pytest", "pytest-cov", "setuptools", "wheel"]},
-    install_requires=["inquirer", "requests"],
+    install_requires=["inquirer==2.8.0", "requests"],
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
