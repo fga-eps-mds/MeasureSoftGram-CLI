b199354589bf6a85381904e7893b609cb558e4a7
Marcos574 <mvdeus574@gmail.com>
2022-03-25 20:52:52 -0300

    Add exceptions to the CLI
    Co-authored-by: VitorB2002 <vitorb.santos2002@gmail.com>

--- src/cli/exceptions.py
+++ src/cli/exceptions.py
@@ -1,28 +1,8 @@
-class MeasureSoftGramCLIException(Exception):
-    """Base MeasureSoftGram CLI Exception"""
-
+class MeasureSoftGramCliException(Exception):
+    """Base MeasureSoftGram Cli exception"""
     pass
 
 
-class InvalidMetricException(MeasureSoftGramCLIException):
-    """Raised when an invalid metric value is provided"""
-
-    pass
-
-
-class InvalidSonarFileAttributeException(MeasureSoftGramCLIException):
-    """Raised when an sonar file attribute is incorrect"""
-
-    pass
-
-
-class InvalidBaseComponentException(MeasureSoftGramCLIException):
-    """Raised when the field baseComponent from sonar metrics file is incorrect"""
-
-    pass
-
-
-class InvalidFileTypeException(MeasureSoftGramCLIException):
-    """' Raised when the file type is not JSON"""
-
+class FileNotFound(MeasureSoftGramCliException):
+    """Raised when a invalid file path is provided to the MeasureSoftGram"""
     pass
