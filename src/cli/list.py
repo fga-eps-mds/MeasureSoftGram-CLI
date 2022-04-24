8aac403c9897d9ea907141eae447825b14692b13
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-24 14:05:46 -0300

    Refactor list command after tests
    Co-authored-by: joao-henrique10 <jhcalzavara@hotmail.com>

--- src/cli/list.py
+++ src/cli/list.py
@@ -1,13 +1,21 @@
 import requests
+import pytz
+from datetime import datetime
 
 BASE_URL = "http://localhost:5000/"
 
 
 def parse_list():
-    pre_configs = requests.get(
+    response = requests.get(
         BASE_URL + "/pre-configs",
         headers={"Accept": "application/json"},
-    ).json()
+    )
+
+    pre_configs = response.json()
+
+    if not 200 <= response.status_code <= 299:
+        print("Error: an error occurred while fetching your pre configurations")
+        return
 
     print(
         "{:<30} {:<35} {:<30} {:<10}".format("ID", "Name", "Created at", "Metrics file")
@@ -16,8 +24,14 @@ def parse_list():
     for pre_config in pre_configs:
         pre_config_name = pre_config["name"] if pre_config["name"] else "-"
 
+        created_at = datetime.fromisoformat(pre_config["created_at"]).astimezone(
+            pytz.timezone("Brazil/East")
+        )
+
+        created_at = created_at.strftime("%m/%d/%Y %H:%M:%S")
+
         print(
             "{:<30} {:<35} {:<30} {:<10}".format(
-                pre_config["_id"], pre_config_name, pre_config["created_at"], ""
+                pre_config["_id"], pre_config_name, created_at, "-"
             )
         )
