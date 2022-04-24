084805d70b865016164ef46020b8e0a190186bf7
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-24 14:03:42 -0300

    Add tests to pre config list
    Co-authored-by: joao-henrique10 <jhcalzavara@hotmail.com>

--- tests/unit/test_list.py
+++ tests/unit/test_list.py
@@ -1,20 +1,89 @@
-import 
+import re
+from io import StringIO
+from src.cli.list import parse_list
 
-# mockar o requests.get -> pra retornar o que vocês desejam
-def test_list_request(self, status_code,mocker)
-    with mocker.patch(
-        "requests.get",
-        return_value = 
-    )
-""" mocker.patch(
-        "requests.get",
-        return_value={"characteristics": ["Usability", "Maintainability"]},
-    ) """
 
-# chamar a função que vocês desejam testar
+class DummyResponse:
+    def __init__(self, status_code):
+        self.status_code = status_code
 
-# mockar o sys.out -> pra ver o que o programa imprime
+    def json(self):
+        return [
+            {
+                "_id": "62656d15f354349ee4abfc7b",
+                "name": "pre-config-1",
+                "created_at": "2022-04-24 15:30:29+00:00",
+            },
+            {
+                "_id": "62656e79f354349ee4abfc7c",
+                "name": "pre-config-2",
+                "created_at": "2022-04-24 15:36:25+00:00",
+            },
+            {
+                "_id": "62656e7ef354349ee4abfc7d",
+                "name": "pre-config-3",
+                "created_at": "2022-04-24 15:36:30+00:00",
+            },
+        ]
 
-# with mocker.patch("sys.stdout", new=StringIO()) as fake_out: 
 
-# fazer os asserts baseados no que vocês esperam que a função retorne
+def test_pre_configs_list(mocker):
+    mocker.patch("requests.get", return_value=DummyResponse(200))
+
+    with mocker.patch("sys.stdout", new=StringIO()) as fake_out:
+        parse_list()
+
+        output_lines = fake_out.getvalue().splitlines()
+
+        assert len(output_lines) == 4
+
+        header_regexp = re.compile(r"ID\s+Name\s+Created at\s+Metrics file")
+
+        assert header_regexp.match(output_lines[0]) is not None
+
+        line_regexp = re.compile(
+            r"(?P<id>[0-9a-z]+)\s+(?P<name>[^\s]+)\s+(?P<created_at>\d{2,}/\d{2,}/\d{4,}\s\d{2,}:\d{2,}:\d{2,})\s+(?P<metrics_file>[^\s]+)"
+        )
+
+        expected_groups = [
+            (
+                "62656d15f354349ee4abfc7b",
+                "pre-config-1",
+                "04/24/2022 12:30:29",
+                "-",
+            ),
+            (
+                "62656e79f354349ee4abfc7c",
+                "pre-config-2",
+                "04/24/2022 12:36:25",
+                "-",
+            ),
+            (
+                "62656e7ef354349ee4abfc7d",
+                "pre-config-3",
+                "04/24/2022 12:36:30",
+                "-",
+            ),
+        ]
+
+        for i in range(1, 4):
+            match_data = line_regexp.match(output_lines[i])
+
+            assert (
+                match_data is not None
+            ), f"output_lines[{i}] does not match the expected line regexp"
+            assert (
+                match_data.groups() == expected_groups[i - 1]
+            ), f"{match_data.groups()} != {expected_groups[i - 1]}"
+
+
+def test_error_in_pre_config_list(mocker):
+    mocker.patch("requests.get", return_value=DummyResponse(500))
+
+    with mocker.patch("sys.stdout", new=StringIO()) as fake_out:
+        parse_list()
+
+        assert (
+            "Error: an error occurred while fetching your pre configurations"
+            in fake_out.getvalue()
+        )
