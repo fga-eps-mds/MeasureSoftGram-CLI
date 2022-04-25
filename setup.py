a336d10852b31d87db517a66ff99fb4c4729848c
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-25 20:31:05 -0300

    Change CLI package version to 2.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="measuresoftgram",
-    version="2.0.0",
+    version="2.0.2",
     extras_require={"dev": ["pytest", "pytest-cov", "setuptools", "wheel"]},
     packages=find_packages(),
     install_requires=["inquirer==2.8.0", "requests"],
