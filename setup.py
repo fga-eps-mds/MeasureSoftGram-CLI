f13c2f822d05b72d33ab0eb3d7d6af638801d9a0
Rhuan Carlos <40495695+Rhuancpq@users.noreply.github.com>
2022-05-06 00:11:09 -0300

    Update version to v2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="measuresoftgram",
-    version="2.0.6",
+    version="2.1.0",
     extras_require={"dev": ["pytest", "pytest-cov", "setuptools", "wheel"]},
     packages=find_packages(),
     install_requires=["inquirer==2.8.0", "requests", "pytz"],
