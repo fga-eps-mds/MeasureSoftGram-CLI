e7c5663c55b74d10d052f1593245d67dbf6cde74
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-07-23 15:30:53 -0300

    #152 Add utils function to check host_url ends with '/'

--- src/cli/utils.py
+++ src/cli/utils.py
@@ -8,3 +8,7 @@ def pretty_date_str(date_str, format="%m/%d/%Y %H:%M:%S", timezone="Brazil/East"
     date_time = date_time.astimezone(pytz.timezone(timezone))
 
     return date_time.strftime(format)
+
+
+def check_host_url(host_url):
+    return host_url if host_url.endswith("/") else host_url + "/"
