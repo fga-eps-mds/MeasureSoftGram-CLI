7472333ba331d71997192706f113532b4801bad2
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-07-23 17:32:21 -0300

    #152 Create utils functions to helper the user experience with measuresoftgram

--- src/cli/utils.py
+++ src/cli/utils.py
@@ -12,3 +12,27 @@ def pretty_date_str(date_str, format="%m/%d/%Y %H:%M:%S", timezone="Brazil/East"
 
 def check_host_url(host_url):
     return host_url if host_url.endswith("/") else host_url + "/"
+
+
+def print_import_files(files):
+    """
+        Importing files:
+            - sonarmetrics/file1.json
+            - sonarmetrics/file2.json
+            - sonarmetrics/file3.json
+
+        Sending the file data:
+    """
+    print('\n\tImporting files:')
+
+    for file in files:
+        print(f'\t\t- {file}')
+
+
+def print_status_import_file(file, message):
+    """
+            - sonarmetrics/file1.json
+            OK: Data sent successfully
+    """
+    print(f'\t\t- {file}')
+    print(f'\t\t{message}')
