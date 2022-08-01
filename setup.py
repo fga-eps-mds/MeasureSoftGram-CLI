076c5e9ec2e72606ce47eddd8d8a85db34d1440a
Durval Carvalho <durval.carvalho@brelo.com.br>
2022-08-01 19:24:36 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="measuresoftgram",
-    version="2.1.1",
+    long_description="CLI da ferramenta MeasureSoftGram"
+    version="3.0.0",
     extras_require={"dev": ["pytest", "pytest-cov", "setuptools", "wheel"]},
     packages=find_packages(),
     install_requires=[
