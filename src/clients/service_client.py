b538592cfda80e97cf0d24a1ca589640e917c037
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-08-12 10:54:08 -0300

    #247 - Create post service client to import file from dir

--- src/clients/service_client.py
+++ src/clients/service_client.py
@@ -7,7 +7,7 @@ class ServiceClient:
     def configure_session(session):
         errors = [500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511]
         retries = Retry(total=5, backoff_factor=5, status_forcelist=errors)
-        session.mount("http://", HTTPAdapter(max_retries=retries))
+        # session.mount("http://", HTTPAdapter(max_retries=retries))
         session.mount("https://", HTTPAdapter(max_retries=retries))
 
     @staticmethod
@@ -17,5 +17,15 @@ class ServiceClient:
             return session.get(url)
 
     @staticmethod
+    def make_post_request(url, payload):
+        with requests.Session() as session:
+            ServiceClient.configure_session(session)
+            return session.post(url, json=payload)
+
+    @staticmethod
     def get_entity(url):
         return ServiceClient.make_get_request(url)
+
+    @staticmethod
+    def import_file(url, payload):
+        return ServiceClient.make_post_request(url, payload)
