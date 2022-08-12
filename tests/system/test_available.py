b2e9d6d1cf210a59353acf818d49669ac8ece69b
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-08-12 10:56:05 -0300

    #247 - Comment tests from cli deprecated commands

--- tests/system/test_available.py
+++ tests/system/test_available.py
@@ -1,17 +1,17 @@
-import subprocess
+# import subprocess
 
 
-def capture(command):
-    proc = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    out, err = proc.communicate()
-    return out, err, proc.returncode
+# def capture(command):
+#     proc = subprocess.Popen(
+#         command,
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#     )
+#     out, err = proc.communicate()
+#     return out, err, proc.returncode
 
 
-def test_available_return_code_invalid():
-    out, _, returncode = capture(["measuresoftgram", "available"])
+# def test_available_return_code_invalid():
+#     out, _, returncode = capture(["measuresoftgram", "available"])
 
-    assert returncode == 1
+#     assert returncode == 1
--- tests/system/test_list.py
+++ tests/system/test_list.py
@@ -1,17 +1,17 @@
-import subprocess
+# import subprocess
 
 
-def capture(command):
-    proc = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    out, err = proc.communicate()
-    return out, err, proc.returncode
+# def capture(command):
+#     proc = subprocess.Popen(
+#         command,
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#     )
+#     out, err = proc.communicate()
+#     return out, err, proc.returncode
 
 
-def test_list_return_code_invalid():
-    out, _, returncode = capture(["measuresoftgram", "list"])
+# def test_list_return_code_invalid():
+#     out, _, returncode = capture(["measuresoftgram", "list"])
 
-    assert returncode == 1
+#     assert returncode == 1
