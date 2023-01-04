d5da0612e9d73c51e04f90b08c78de84d14fd86a
RcleydsonR <rafael.cleydson@gmail.com>
2023-01-04 19:31:16 -0300

    Modify setup.py to change from measuresoftgram to msgram and also change version

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="measuresoftgram",
+    name="msgram",
     long_description="CLI da ferramenta MeasureSoftGram",
-    version="3.4.6",
+    version="0.1.0",
     extras_require={"dev": ["pytest", "pytest-cov", "setuptools", "wheel"]},
     packages=find_packages(),
     install_requires=[
-        "inquirer==2.8.0",
         "requests==2.28.1",
         "pytz",
+        "typing~=3.7.4.3",
         "tabulate==0.8.10",
         "termcolor==1.1.0",
         "pandas~=1.4.4",
+        "setuptools~=60.2.0",
+        "python-dotenv",
+        "rich",
+        "msgram-core==0.1.0",
         "validators==0.20.0",
     ],
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={"console_scripts": ["measuresoftgram=src.cli.cliRunner:main"]},
+    entry_points={"console_scripts": ["msgram=src.cli.cliRunner:main"]},
 )
