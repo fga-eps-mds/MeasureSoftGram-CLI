0be5d1eba2aa94b80b73f8923a45e061c400712d
RcleydsonR <rafael.cleydson@gmail.com>
2023-01-14 11:48:40 -0300

    Add cli return of calculated characteristics from calculate command
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>
    Co-authored-by: flaviovl <flavio.vl@gmail.com>
    Co-authored-by: francisco1code <francisco1code@gmail.com>

--- src/cli/commands/cmd_calculate.py
+++ src/cli/commands/cmd_calculate.py
@@ -8,7 +8,7 @@ from src.cli.jsonReader import open_json_file
 from src.cli.exceptions import MeasureSoftGramCLIException
 from src.cli.resources.measure import calculate_measures
 from src.cli.resources.subcharacteristic import calculate_subcharacteristics
-
+from src.cli.resources.characteristic import calculate_characteristics
 logger = logging.getLogger("msgram")
 
 
@@ -26,18 +26,19 @@ def command_calculate(args):
     try:
         data_measures, headers_measures = calculate_measures(file_path)
         data_subcharacteristics, headers_subcharacteristics = calculate_subcharacteristics(config, data_measures['measures'])
-        # data_characteristics, headers_characteristics = calculate_characteristics(host_url)
+        data_characteristics, headers_characteristics = calculate_characteristics(config, data_subcharacteristics['subcharacteristics'])
         # data_sqc, headers_sqc = calculate_sqc(host_url)
 
         if output_format == "tabular":
             print(colored("\nCalculated Measures: \n", "green"))
             print(tabulate(data_measures, headers=headers_measures))
 
-            # print(colored("\nCalculated Characteristics: \n", "green"))
-            # print(tabulate(data_characteristics, headers=headers_characteristics))
-
             print(colored("\nCalculated Subcharacteristics: \n", "green"))
             print(tabulate(data_subcharacteristics, headers=headers_subcharacteristics))
+
+            
+            print(colored("\nCalculated Characteristics: \n", "green"))
+            print(tabulate(data_characteristics, headers=headers_characteristics))
 
             # print(colored("\nCalculated SQC: \n", "green"))
             # print(tabulate(data_sqc, headers=headers_sqc))
@@ -46,11 +47,12 @@ def command_calculate(args):
             print(colored("\nCalculated Measures: \n", "green"))
             print(json.dumps(data_measures))
 
-            # print(colored("\nCalculated Characteristics: \n", "green"))
-            # print(json.dumps(data_characteristics))
-
             print(colored("\nCalculated Subcharacteristics: \n", "green"))
             print(json.dumps(data_subcharacteristics))
+
+            
+            print(colored("\nCalculated Characteristics: \n", "green"))
+            print(json.dumps(data_characteristics))
 
             # print(colored("\nCalculated SQC: \n", "green"))
             # print(json.dumps(data_sqc))
