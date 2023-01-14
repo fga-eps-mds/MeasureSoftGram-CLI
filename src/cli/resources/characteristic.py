50bb5fce11bd768e65ce4771568efd1a6be4c3a9
RcleydsonR <rafael.cleydson@gmail.com>
2023-01-14 15:42:32 -0300

    Add get characteristic value method to return it key, value and weight
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>
    Co-authored-by: flaviovl <flavio.vl@gmail.com>
    Co-authored-by: francisco1code <francisco1code@gmail.com>

--- src/cli/resources/characteristic.py
+++ src/cli/resources/characteristic.py
@@ -3,6 +3,20 @@ from src.cli.resources.subcharacteristic import get_subcharacteristic_value
 from resources import calculate_characteristics as core_calculate
 
 
+def get_characteristic_value(chars, sqc):
+    char_calculated = []
+    for char in sqc:
+        char_key = char["key"]
+
+        char_calculated.append({
+            "key": char_key,
+            "value": {m['key']: m['value'] for m in chars}[char_key],
+            "weight": char['weight'],
+        })
+
+    return char_calculated
+
+
 def calculate_characteristics(config, subchars):
     characteristics = config['characteristics']
     calculate_infos = []
