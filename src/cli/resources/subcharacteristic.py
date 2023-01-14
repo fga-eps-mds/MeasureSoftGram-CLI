0947dc83067ebb175fe373e1f1ab0c0e73a0096b
RcleydsonR <rafael.cleydson@gmail.com>
2023-01-14 11:46:13 -0300

    Add get subcharacteristic value method to return it key, value and weight
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>
    Co-authored-by: flaviovl <flavio.vl@gmail.com>
    Co-authored-by: francisco1code <francisco1code@gmail.com>

--- src/cli/resources/subcharacteristic.py
+++ src/cli/resources/subcharacteristic.py
@@ -2,13 +2,23 @@ from src.cli.resources.measure import get_measure_value
 
 from resources import calculate_subcharacteristics as core_calculate
 
+def get_subcharacteristic_value(subchars, char):
+    subchar_calculated = []
+    for subchar in char:
+        subchar_key = subchar["key"]
+
+        subchar_calculated.append({
+            "key": subchar_key,
+            "value": {m['key']: m['value'] for m in subchars}[subchar_key],
+            "weight": subchar['weight'],
+        })
+
+    return subchar_calculated
 
 def calculate_subcharacteristics(config, measures):
     subchars = [sc['subcharacteristics'] for sc in config['characteristics']]
     calculate_infos = []
 
-    # from rich.traceback import install
-    # install(show_locals=True)
     for subchar in subchars:
         calculate_infos.append({
             'key': subchar[0]['key'],
