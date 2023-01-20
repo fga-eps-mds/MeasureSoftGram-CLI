aace06452cb87729d38cb9af3ebe25371fec0d08
Flavio Vieira Leao <flavio.vl@gmail.com>
2023-01-20 08:31:54 -0300

    add functions to standardize writing menssages in the  terminal with color and style
    Co-authored-by: João Moura <jaummoura15@gmail.com>
    Co-authored-by: Rafael Ramos <rafael.cleydson@gmail.com>

--- src/cli/utils.py
+++ src/cli/utils.py
@@ -1,49 +1,105 @@
-import pytz
 import logging
-
-from termcolor import colored
 from datetime import datetime
 
+import pytz
+from rich import box, print
+from rich.console import Console
+from rich.panel import Panel
+from rich.progress import BarColumn, Progress, TaskProgressColumn, TextColumn
+from rich.table import Table
+
 logger = logging.getLogger("msgram")
+console = Console(highlight=False, soft_wrap=False, width=140)
 
 
-def pretty_date_str(date_str, format="%m/%d/%Y %H:%M:%S", timezone="Brazil/East"):
-    date_time = datetime.fromisoformat(date_str)
-
+def pretty_date_str(date: str, format="%m/%d/%Y %H:%M:%S", timezone="Brazil/East"):
+    date_time = datetime.fromisoformat(date)
     date_time = date_time.astimezone(pytz.timezone(timezone))
 
     return date_time.strftime(format)
 
 
-def check_host_url(host_url):
-    return host_url if host_url.endswith("/") else host_url + "/"
+def print_info(text: str):
+    """Print an info message."""
+    console.print(text, style="green")
 
 
-def print_import_files(files):
-    """
-    Importing files:
-        - sonarmetrics/file1.json
-        - sonarmetrics/file2.json
-        - sonarmetrics/file3.json
-
-    Sending the file data:
-    """
-    logger.info("\n\tImporting files:")
-
-    for file in files:
-        logger.info(f"\t\t- {file}")
-
-    logger.info("\n\tSending the file data:")
+def print_warn(text: str):
+    """Print a warning message."""
+    console.print(text, style="yellow")
 
 
-def print_status_import_file(file, message, trying_idx):
-    """
-    - sonarmetrics/file1.json
-    OK: Data sent successfully
-    """
-    print(f"\t\t- [attempt n° {trying_idx}] {file}")
+def print_error(text: str):
+    """Print a error message."""
+    console.print(text, style="red")
 
-    if "OK:" in message:
-        print(colored(f"\t\t\t{message}\n", "green"))
+
+def print_table(the_dict: dict, table_name: str | None, field: str, col_value: str):
+
+    table = Table(
+        title=f"{table_name or ''}",
+        title_style="bold",
+        row_styles=["none", "dim"],
+        border_style="bright_yellow",
+        pad_edge=False,
+        box=box.MINIMAL,
+    )
+
+    table.add_column(
+        field,
+        no_wrap=True,
+        header_style="bold cyan",
+        footer_style="bright_cian",
+        style="cyan",
+    )
+
+    table.add_column(
+        field,
+        no_wrap=True,
+        header_style="bold red",
+        footer_style="bright_red",
+        style="red",
+    )
+
+    for field, value in the_dict.items():
+        table.add_row(str(field), str(value))
+
+    console.print(table)
+
+
+def make_progress_bar() -> Progress:
+    progress_bar = Progress(
+        TextColumn("{task.description}"),
+        TextColumn("[bold bright_red]Waiting  "),
+        BarColumn(complete_style="red"),
+        TaskProgressColumn(),
+        refresh_per_second=10,
+        transient=True,
+    )
+    return progress_bar
+
+
+def print_rule(title: str | None, text: str | None, style: str = "#4682B4"):
+    if title:
+        console.rule(f"{title}", style=style)
     else:
-        print(colored(f"\t\t\t{message}\n", "red"))
+        console.rule(style=style)
+    if text:
+        console.print(text, style="grey58")
+
+    console.line()
+
+
+def print_panel(menssage: str):
+    console.line(2)
+    print(
+        Panel(
+            menssage,
+            title="Next steps",
+            title_align="center",
+            style="#4F4F4F",
+            border_style="#A9A9A9",
+            padding=(0, 2),
+            width=140,
+        ),
+    )
