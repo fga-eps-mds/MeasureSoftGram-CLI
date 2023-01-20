7f4c40a29d591b926fcd07b7cb0c1bfa640af07c
Flavio Vieira Leao <flavio.vl@gmail.com>
2023-01-20 08:54:10 -0300

    add custom messagens in command init
    Co-authored-by: João Moura <jaummoura15@gmail.com>
    Co-authored-by: Rafael Ramos <rafael.cleydson@gmail.com>

--- src/cli/commands/cmd_init.py
+++ src/cli/commands/cmd_init.py
@@ -1,13 +1,13 @@
+import json
 import logging
 import sys
-import json
 from pathlib import Path
-from rich import print
-from rich.panel import Panel
+
 from rich.console import Console
 from rich.prompt import Confirm
-
 from staticfiles import DEFAULT_PRE_CONFIG
+
+from src.cli.utils import print_error, print_info, print_panel, print_rule
 from src.config.settings import FILE_CONFIG
 
 logger = logging.getLogger("msgram")
@@ -19,55 +19,38 @@ def command_init(args):
 
     except Exception as e:
         logger.error(f"KeyError: args[{e}] - non-existent parameters")
+        print_error(f"KeyError: args[{e}] - non-existent parameters")
         sys.exit(1)
 
     logger.debug(config_path)
-    console = Console()
     file_path = config_path / FILE_CONFIG
 
+    console = Console()
     console.clear()
-    console.rule(title="MSGram", style="#4682B4")
-    print("[#708090]Init to set config file[/]:")
-    console.line(1)
+    print_rule("MSGram", "[#708090]Init to set config file[/]:")
 
     if not config_path.exists():
-        print(f"create dir: {config_path}")
+        print_info(f"Created dir: {config_path}")
         config_path.mkdir()
 
     replace = True
 
     if file_path.exists():
-        print(f"MSGram config file [bold red]'{FILE_CONFIG}'[/] exists already!")
+        print_info(f"MSGram config file [bold red]'{FILE_CONFIG}'[/] exists already!")
         replace = Confirm.ask(f"> Do you want to replace [bold blue]'{FILE_CONFIG}'[/]?")
 
     if replace:
         try:
             with file_path.open("w") as f:
                 f.write(json.dumps(DEFAULT_PRE_CONFIG, indent=4))
-
         except OSError:
             console.line(2)
-            print("[bold red]Error opening or writing to file[/]")
-
-        console.print(
-            f"The file config: '{config_path.name}/msgram.json' was created successfully.",
-            style="green",
-        )
+            print_error("Error opening or writing to file")
+        print_info(f"The file config: '{config_path.name}/msgram.json' was created successfully.")
 
     else:
-        console.print(
-            f"The file config: '{config_path.name}/msgram.json' not changed...",
-            style="green",
-        )
+        print_info(f"The file config: '{config_path.name}/msgram.json' not changed...")
 
-    console.line(2)
-    print(
-        Panel(
-            "> [#008080]Run msgram extract -o sonarqube -dp data_path[/], to extract suported metrics!",
-            title="Next steps",
-            title_align="center",
-            style="#4F4F4F",
-            border_style="#A9A9A9",
-            padding=(1, 2),
-        ),
+    print_panel(
+        "> [#008080]Run msgram extract -o sonarqube -dp data_path[/], to extract suported metrics!"
     )
