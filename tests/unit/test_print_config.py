dfd0602da0694f47b22e8a1a7129165a4a81ea9c
ArielSixwings <arielserafim@gmail.com>
2023-10-22 19:51:21 -0300

    test: Add test_print_config
    Co-Authored-by: Ana Carolina Rodrigues <anacarolinarodrigues480@gmail.com>

--- tests/unit/test_print_config.py
+++ tests/unit/test_print_config.py
@@ -7,10 +7,11 @@ import json
 from src.cli.commands.cmd_print_config import print_json_tree
 from src.cli.utils import  print_info,  print_rule
 
+import re
     
 def test_print_json_tree():
 
-    file = open("tests/unit/data/msgram.json")
+    file = open("tests/unit/data/newmsgram.json")
     data = json.load(file)
 
     captured_output = StringIO()
@@ -23,5 +24,13 @@ def test_print_json_tree():
     fileExpected = open("tests/unit/data/expected_list.txt")
 
     compare = fileExpected.read()
+
+    # O padrão de regex para cores no formato [#FFFFFF] e [#458B00]
+    color_pattern = r'\[#\w+\]'
+
+    # Substituir todas as ocorrências do padrão pelo texto vazio
+    result = re.sub(color_pattern, '', result)
+    result = re.sub('\n', '', result)
+    compare = re.sub('\n', '', compare)
 
     assert result == compare
