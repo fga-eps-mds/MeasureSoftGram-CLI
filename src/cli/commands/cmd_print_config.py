7f013447bec31a264e279399dbdb5adf55bcad1d
Ana Carolina Rodrigues <anacarolinarodrigues480@gmail.com>
2023-10-22 15:26:28 -0300

    Fix: Add indent

--- src/cli/commands/cmd_print_config.py
+++ src/cli/commands/cmd_print_config.py
@@ -13,64 +13,59 @@ from src.config.settings import FILE_CONFIG
 from src.config.settings import DEFAULT_CONFIG_FILE_PATH
 from src.config.settings import DEFAULT_CONFIG_PATH
 
-logger = logging.getLogger("msgram")
-
-def print_json_tree(data, indent="",isTop = True):
+def print_json_tree(data, indent="", isTop = True):
     key = data.get("key")
+    value = data.get("metric")
 
     if isTop:
-        print_info("\n\n")
-        print_info("---------------------------- Listing Configuration Parameters ---------------------------\n")   
-        print_info(f"[#FFFFFF]{indent}[#458B00]{key}")
+        print_info(f"[#FFFFFF]\nCaracterística:")
+        print_info(f"[#FFFFFF]{indent}{indent}[#458B00]{key}")
     else:
-        print_info(f"[#FFFFFF]{indent}[#458B00]{key}")
+        print_info(f"[#FFFFFF]{indent}{indent}[#458B00]{key}")
    
     weight = data.get("weight", 0)
-    print_info(f"[#FFFFFF]{indent}Peso: [#458B00]{weight}%")
+    print_info(f"[#FFFFFF]{indent}{indent}Peso: [#458B00]{weight}%")
 
     if "subcharacteristics" in data:
         for subchar in data["subcharacteristics"]:
             print_info(f"[#FFFFFF]{indent}Subcaracteristica (s):")
-            print_json_tree(subchar, indent + "|     ",False)
-            print_info(f"[#FFFFFF]{indent}Fim-Subcaracteristica(s)\n")
+            print_json_tree(subchar, indent + "|  ",False)
+            print_info(f"[#FFFFFF]{indent}{indent}Fim-Subcaracterística\n")
 
     if "measures" in data:
         for measure in data["measures"]:
             print_info(f"[#FFFFFF]{indent}{indent}Medida (s):")
-            print_json_tree(measure, indent + "|     |      ",False)
+            print_json_tree(measure, indent + "|  ",False)
             print_info(f"[#FFFFFF]{indent}{indent}Fim-Medida(s)\n")
 
     if "min_threshold" in data and "max_threshold" in data:
-        min_threshold = data.get("min_threshold")
-        max_threshold = data.get("max_threshold")
-        print_info(f"[#FFFFFF]{indent}Metrica (s):")
-        print_info(f"[#FFFFFF]{indent}|      [#458B00]coverage")
-        print_info(f"[#FFFFFF]{indent}|      Peso: [#458B00]{weight}%")
-        print_info(f"[#FFFFFF]{indent}|      Valores de referência: Min: [#458B00]{min_threshold} [#FFFFFF]e Max: [#458B00]{max_threshold}")
+            print_info(f"[#FFFFFF]{indent}{indent}Métrica (s):")
+            value = data.get("metric")
+            #print_info("{indent} |     |     |      {value}")
+            min_threshold = data.get("min_threshold")
+            max_threshold = data.get("max_threshold")
+            print_info(f"[#FFFFFF]{indent}|    Valores de referência: Min: [#458B00]{min_threshold} [#FFFFFF]e Max: [#458B00]{max_threshold}")
+            print_info(f"[#FFFFFF]{indent}{indent}Fim-Metrica(s)\n")
 
 def command_list_config(args):
-    
     console = Console()
     console.clear()
 
-    print_rule("MSGram", "[#708090] Reading of config file[/]:")
+    print_rule("[#FFFFFF] Listing Configuration Parameters[/]:")
 
     if not (os.path.exists(DEFAULT_CONFIG_FILE_PATH)):
         print_info( f"[#A9A9A9] O arquivo de configuração não foi encontrado. Execute o comando msgram init para criá-lo." )
         exit()
-    
-    print_info(f"MSGram config file [bold red]'{FILE_CONFIG}'[/] exists already!")
+    else:
+        print_info(f"MSGram config file [bold red]'{FILE_CONFIG}'[/] exists already!")
 
-    f = open(DEFAULT_CONFIG_FILE_PATH)
-    data = json.load(f)
+        f = open(DEFAULT_CONFIG_FILE_PATH)
+        data = json.load(f)
 
+        isTop = True
+        for data in data.get("characteristics", []):
+            print_json_tree(data, "", isTop)
+            print_info(f"[#FFFFFF]Fim-Característica\n")
+            isTop = False
 
-    isTop = True
-    for characteristic in data.get("characteristics", []):
-        print_json_tree(characteristic,"",isTop)
-        print_info(f"[#FFFFFF]Fim-Caracteristica(s)\n")
-        isTop = False
-
-
-
-    print_info("\n[#A9A9A9]Para editar o arquivo de configuração utilize em seu terminal o seguinte comando: vim <caminho_arquivo ../.msgram/.msgram/msgram.json>")
+        print_info("\n[#A9A9A9]Para editar o arquivo de configuração utilize em seu terminal o seguinte comando: vim <caminho_arquivo ../.msgram/.msgram/msgram.json>")
