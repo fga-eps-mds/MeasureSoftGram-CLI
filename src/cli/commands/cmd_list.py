bbf769605505d6bce4db457d094cacc2b5d28f5a
ArielSixwings <arielserafim@gmail.com>
2023-11-30 10:49:06 -0300

    Feat: initial steps to implement new measue team throuput
    Co-Authored-by: Ana Carolina Rodrigues <anacarolinarodrigues480@gmail.com>

--- src/cli/commands/cmd_list.py
+++ src/cli/commands/cmd_list.py
@@ -20,7 +20,8 @@ def print_json_tree(data):
     measure_to_metric["test_coverage"] = ['coverage']
     measure_to_metric["non_complex_file_density"] = ['complexity_functions', 'total_number_of_files']
     measure_to_metric["commented_file_density"] = ['commented_lines_density']
-    measure_to_metric["duplication_absence"] = ['duplication_lines_density']
+    measure_to_metric["duplication_absense"] = ['duplication_lines_density']
+    measure_to_metric["team_throughput"] = ['resolved_issues', 'total_issues']
 
     while stack:
         data, indent = stack.pop()
